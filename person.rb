52f2b29f0e96d57fc339243883b9056e45289326
lily-coder <lilyondie@gmail.com>
2022-03-05 05:39:19 +0300

    Add class teacher

--- person.rb
+++ person.rb
@@ -1,25 +1,24 @@
 class Person
-    attr_accessor :age, :name
+  attr_accessor :age, :name
 
-    attr_reader :id
+  attr_reader :id
 
-    def initialize(age, name = "unknown", parent_permission = true)
-        @id = Random.rand(1..1000)
-        @age = age
-        @name = name
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'unknown', parent_permission = true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
 
-    private 
-      
-    def is_of_age?
-        @age >= 18
-    end
+  private
 
-    public
+  def is_of_age?
+    @age >= 18
+  end
 
-    def can_use_services? 
-        is_of_age?() || @parent_permission
-    end
-    
+  public
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 end
