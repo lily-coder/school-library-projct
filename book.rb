bfaf98a2c82d853bbf39aaaa742aa1f01d668775
lily-coder <lilyondie@gmail.com>
2022-03-09 11:08:45 +0300

    Refactor code

--- book.rb
+++ book.rb
@@ -1,13 +1,14 @@
 class Book
-  attr_accessor :title, :author, :rentals
+  attr_reader :rentals
+  attr_accessor :title, :author
 
-  def initialize(title, author)
+  def initialize(title:, author:)
     @title = title
     @author = author
     @rentals = []
   end
 
-  def add_rental(person, date)
-    Rental.new(person, date, self)
+  def add_rental(rental)
+    @rentals << rental
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,11 +1,22 @@
 class Rental
-  attr_accessor :date, :person, :book
+  attr_accessor :date
+  attr_reader :person, :book
 
-  def initialize(date, person, book)
+  def initialize(date:, person:, book:)
     @date = date
     @book = book
     book.rentals << self
     @person = person
     person.rentals << self
+  end
+
+  def book=(book)
+    @book = book
+    book.add_rental(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.add_rental(self)
   end
 end
